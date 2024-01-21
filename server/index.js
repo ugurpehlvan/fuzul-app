15a054e2d218c00e2f4f0549d015af0bb6b2204e
ugurpehlvan <hireugurpehlivan@gmail.com>
2024-01-22 01:45:18 +0300

    Deploy app to Heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 import path from 'path';
 
 // routes
@@ -13,12 +14,22 @@ app.use(cors());
 // NodeJS serve the files for our built React app
 const __dirname = path.resolve();
 
+const buildPath = path.resolve(__dirname, '../client/build');
+
+fs.access(buildPath, fs.constants.F_OK, (err) => {
+  if (err) {
+    console.error(`Directory does not exist: ${buildPath}`);
+  } else {
+    console.log(`Directory exists: ${buildPath}`);
+  }
+});
+
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use("/api/properties", propertyRoutes);
 
 // Catch-all handler for client-side routing
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
