99746389481846912a28011ed4d460c5451c8ad4
ugurpehlvan <hireugurpehlivan@gmail.com>
2024-01-21 04:38:45 +0300

    Filter data

--- client/src/pages/properties.js
+++ client/src/pages/properties.js
@@ -6,6 +6,16 @@ import axios from 'axios';
 export default function Properties() {
     const [properties, setProperties] = useState([]);
 
+    const handlePropertiesFilter = () => {
+        axios.get('/api/properties/search?location=City&priceRange=10,500000&propertyType=Apartment')
+            .then(({ data }) => {
+              console.log("data", data);
+                // setProperties(data);
+            })
+            .catch((error) => console.error('Error fetching data:', error));
+    }
+
+
   useEffect(() => {
     axios.get('/api/properties')
       .then(({ data }) => {
@@ -13,10 +23,11 @@ export default function Properties() {
       })
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
-  
+
   return (
     <div>
       <h2>Property List</h2>
+      <button onClick={handlePropertiesFilter}>Filter</button>
       <ul>
         {properties.map((property) => (
           <li key={property.id}>
