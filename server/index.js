98d5f897c2106b66b6dbee2833c43c4b3acf763a
ugurpehlvan <hireugurpehlivan@gmail.com>
2024-01-21 16:58:48 +0300

    remove unnecassary deps

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import { dirname } from 'path';
 
 // routes
 import propertyRoutes from './routes/properties.js';
